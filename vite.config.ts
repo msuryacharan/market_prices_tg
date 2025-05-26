da03332b52eddd2264b3505f89dcce62d6901e95
msuryacharan <msuryacharan5@gmail.com>
2025-05-26 08:08:35 +0530

    updated files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/market_prices_tg/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
